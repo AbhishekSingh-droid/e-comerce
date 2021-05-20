75bbcbb4fd781ee01c510f099d57d1cd097bce8c
AbhishekSingh-droid <65038138+AbhishekSingh-droid@users.noreply.github.com>
2021-05-20 21:31:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+//import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./components/authFile/SignIn";
